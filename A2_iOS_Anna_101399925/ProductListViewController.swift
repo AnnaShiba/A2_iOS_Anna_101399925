b5704945d99d15f752831828419253504735b01f
Anna Shibanova <anna.shibanova@georgebrown.ca>
2025-03-27 00:02:38 -0500

    Implement the controller, fix layout

--- A2_iOS_Anna_101399925/ProductListViewController.swift
+++ A2_iOS_Anna_101399925/ProductListViewController.swift
@@ -8,6 +8,41 @@
 import UIKit
 import CoreData
 
-class ProductListViewController: UIViewController {
+class ProductListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
     
+    var products: [Product] = []
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        fetchProducts()
+    }
+
+    func fetchProducts() {
+        let request: NSFetchRequest<Product> = Product.fetchRequest()
+        do {
+            products = try context.fetch(request)
+            tableView.reloadData()
+        } catch {
+            print("Failed to fetch products: \(error)")
+        }
+    }
+
+    // MARK: - TableView Data Source
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return products.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ProductCell", for: indexPath)
+        let product = products[indexPath.row]
+        cell.textLabel?.text = product.name
+        cell.detailTextLabel?.text = product.desc
+        return cell
+    }
 }
