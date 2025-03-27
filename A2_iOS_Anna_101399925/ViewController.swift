22017eb2f1564396524607763db1955a094dd4bc
Anna Shibanova <anna.shibanova@georgebrown.ca>
2025-03-26 21:10:57 -0500

    Initial Commit

---
+++ A2_iOS_Anna_101399925/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A2_iOS_Anna_101399925
+//
+//  Created by Anna on 26.03.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
