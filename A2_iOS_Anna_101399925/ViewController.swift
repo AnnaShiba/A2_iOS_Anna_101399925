f55196cbf08879f1edc0c85db330fce7397f252f
Anna Shibanova <anna.shibanova@georgebrown.ca>
2025-03-26 21:59:55 -0500

    Connect outlets to UI

--- A2_iOS_Anna_101399925/ViewController.swift
+++ A2_iOS_Anna_101399925/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var providerLabel: UILabel!
+    
+    @IBOutlet weak var prevButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateView()
     }
 
+    func updateView() {
+        nameLabel.text = "No Products"
+    }
 
 }
 
